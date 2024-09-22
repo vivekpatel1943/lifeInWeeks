a1c4ede0d3a3dcd5df068013e7fa85c8d919ac88
Vivek Patel <vivekpatel496115@gmail.com>
2024-09-22 14:41:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const lifeForm = document.getElementById("lifeForm");
 const displayBoardContainer = document.getElementById('displayBoardContainer');
-/* const lifeInYearsBtn = document.getElementById('lifeInYearsBtn');
-const lifeInMonthsBtn = document.getElementById('lifeInYearsBtn');
-const lifeInWeeksBtn = document.getElementById('lifeInYearsBtn'); */
+
 
 
 const buttons = document.getElementsByClassName('button');
-console.log(buttons);
-console.log(buttons.length);
+
 const yrsBtn = document.getElementById('lifeInYearsBtn');
 const mnthsBtn = document.getElementById('lifeInMonthsBtn');
 const weeksBtn = document.getElementById('lifeInWeeksBtn');
 
-// console.log(screenwidth);
 
-// function lifeBoard(lifeExpectation){
-
-// }
 window.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
-        let lifeExpectation = parseInt(document.getElementById('lifeExpectation').value);
-        let currentAge = parseInt(document.getElementById('currentAge').value);
+     
+        let birthday = document.getElementById('birthdate').value;
+        birthday = new Date(birthday);
+
+        let birthTime = birthday.getTime();
+
+        let lifeExpectancy = document.getElementById('lifeExpectancy').value;
+        lifeExpectancy = parseInt(lifeExpectancy);
+        let deathYear = birthday.getFullYear() + 90;
+        let deathDay = new Date(`${birthday.getDay} ${birthday.getMonth} ${deathYear}`);
+        let deathTime = deathDay.getTime();
+        let presentDate = new Date();
+        let presentTime = presentDate.getTime();
+
+        let lifeTime  = deathTime - birthTime;
+        let lifeTimeInYears = Math.floor(lifeTime / (1000* 60*60*24*365)); 
+        let lifeTimeInMonths = Math.floor(lifeTime / (1000* 60*60*24*30)); 
+        let lifeTimeInWeeks = Math.floor(lifeTime / (1000* 60*60*24*7));  
+
+        let lifeLived = presentTime - birthTime; 
+        let lifeLivedInYears = Math.floor(lifeLived / (1000* 60*60*24*365)); 
+        let lifeLivedInMonths = Math.floor(lifeLived / (1000* 60*60*24*30)); 
+        let lifeLivedInWeeks = Math.floor(lifeLived / (1000* 60*60*24*7)); 
+
+        let lifeLeft = deathTime - presentTime;
+        let lifeLeftInYears = Math.floor(lifeLeft / (1000* 60*60*24*365)); 
+        let lifeLeftInMonths = Math.floor(lifeLeft/ (1000* 60*60*24*30)); 
+        let lifeLeftInWeeks = Math.floor(lifeLeft / (1000* 60*60*24*7)); 
+      
 
         let rows = 0;
         let cols = 0;
         let cellSize = 0; //new variable for controlling cell Size
         let fontSize = 0;
-        let lifeExpectationInWeeks = 0;
-        let lifeExpectationInMonths = 0;
-        let lifeExpectationInYears = 0;
-        let currentAgeInYears = 0;
-        let currentAgeInMonths = 0;
-        let currentAgeInWeeks = 0;
+       
         
 
         // Clear the board container before creating a new board
@@ -54,41 +69,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 // Check which button is active and set rows and cols accordingly
                 if (yrsBtn.classList.contains('BtnActive')) {
-                    lifeExpectationInYears = lifeExpectation * 1;
-                    currentAgeInYears = currentAge * 1;
-                    cols = Math.floor(lifeExpectationInYears / 10);
-                    rows = Math.floor(lifeExpectationInYears / 1);;
+                    // lifeExpectationInYears = lifeExpectation * 1;
+                    // currentAgeInYears = currentAge * 1;
+                    cols = Math.floor(lifeTimeInYears / 10);
+                    rows = Math.floor(lifeTimeInYears / 1);;
                     cellSize = 10;
                     fontSize = 15;
                     if(screenwidth < 600){
-                        cols = Math.floor(lifeExpectationInYears / 20);
-                        rows = Math.floor(lifeExpectationInYears / 1);;
+                        cols = Math.floor(lifeTimeInYears / 20);
+                        rows = Math.floor(lifeTimeInYears / 1);;
                     }
-                    colorBoard(lifeExpectationInYears, currentAgeInYears)
+                    colorBoard(lifeTimeInYears, lifeLivedInYears)
                 } else if (mnthsBtn.classList.contains('BtnActive')) {
-                    lifeExpectationInMonths = lifeExpectation * 12;
-                    currentAgeInMonths = currentAge * 12;
-                    cols = Math.floor(lifeExpectationInMonths / 20);
-                    rows = Math.floor(lifeExpectationInMonths / 10);
+                    // lifeTimeInMonths = lifeExpectation * 12;
+                    // currentAgeInMonths = currentAge * 12;
+                    cols = Math.floor(lifeTimeInMonths / 30);
+                    rows = Math.floor(lifeTimeInMonths / 5);
                     cellSize = 5;
                     fontSize = 10;
                     if(screenwidth <= 600){
-                        cols = Math.floor(lifeExpectationInMonths / 120);
-                        rows = Math.floor(lifeExpectationInMonths / 5);;
+                        cols = Math.floor(lifeTimeInMonths / 120);
+                        rows = Math.floor(lifeTimeInMonths / 20);
                     }
-                    colorBoard(lifeExpectationInMonths, currentAgeInMonths)
+                    colorBoard(lifeTimeInMonths,lifeLivedInMonths)
                 } else if (weeksBtn.classList.contains('BtnActive')) {
-                    lifeExpectationInWeeks = lifeExpectation * 52;
-                    currentAgeInWeeks = currentAge * 52;
-                    cols = Math.floor(lifeExpectationInWeeks / 60);
-                    rows = Math.floor(lifeExpectationInWeeks / 10);
+                    
+                    cols = Math.floor(lifeTimeInWeeks/ 100);
+                    rows = Math.floor(lifeTimeInWeeks/ 10);
                     cellSize = 1;
                     fontSize = 10;
                     if(screenwidth < 600){
-                        cols = Math.floor(lifeExpectationInWeeks / 400);
-                        rows = Math.floor(lifeExpectationInWeeks / 5);
+                        cols = Math.floor(lifeTimeInWeeks / 400);
+                        rows = Math.floor(lifeTimeInWeeks / 5);
                     }
-                    colorBoard(lifeExpectationInWeeks, currentAgeInWeeks)
+                    colorBoard(lifeTimeInWeeks, lifeLivedInWeeks)
 
                 }
 
@@ -97,7 +111,7 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
 
-        function colorBoard(lifeExpectationTime, currentAgeTime) {
+        function colorBoard(lifeExpectancyTime, lifeLivedTime) {
             // Create the board dynamically using a table
             let table = document.createElement('table');
             table.style.borderCollapse = 'collapse';
@@ -106,16 +120,16 @@ window.addEventListener('DOMContentLoaded', () => {
                 for (let j = 1; j <= cols; j++) {
                     let cell = document.createElement('td');
                     cell.textContent = i * cols + j;
-                    if (parseInt(cell.textContent) > lifeExpectationTime) {
+                    if (parseInt(cell.textContent) > lifeExpectancyTime) {
                         cell.style.display = 'none';
                     }
-                    if (parseInt(cell.textContent) < currentAgeTime) {
+                    if (parseInt(cell.textContent) < lifeLivedTime) {
                         cell.style.backgroundColor = 'red';
                     }
-                    if (parseInt(cell.textContent) > currentAgeTime) {
+                    if (parseInt(cell.textContent) > lifeLivedTime) {
                         cell.style.backgroundColor = 'green';
                     }
-                    if (parseInt(cell.textContent) === currentAgeTime) {
+                    if (parseInt(cell.textContent) === lifeLivedTime) {
                         cell.style.backgroundColor = 'blue';
                     }
 
